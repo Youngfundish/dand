f19f2cd96cfc0d4923c191b5a3b5648ae9cfb8ca
Youngfundish <funderchristian@gmail.com>
2020-03-04 21:21:42 +0100

    commit

---
+++ src/components/EditorApp.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import EditorNav from './editor/layout/EditorNav';
+
+
+const EditorApp = () => {
+   
+        return (  
+            <EditorNav />
+        );
+    }
+
+ 
+export default EditorApp;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom"
+
 import App from './App';
+import EditorApp from './components/editor/EditorApp.js'
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+<Router>
+    <Switch>
+        <Route path="/" component={App}/>
+    </Switch>
+</Router>
+    , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
